05f308bc527c2fc5ce4e1373f83effec3a983624
liuqixing <liuqixing@gmail.com>
2018-05-29 14:39:38 +0800

    sqlit_migrations.js

--- signature.js
+++ signature.js
@@ -1,22 +1,39 @@
 /*jslint node: true */
 "use strict";
 
-var log			= require( './log.js' );
-var ecdsa = require('secp256k1');
+var ecdsa	= require( 'secp256k1' );
+var log		= require( './log.js' );
 
-exports.sign = function(hash, priv_key){
-	var res = ecdsa.sign(hash, priv_key);
-	return res.signature.toString("base64");
-};
 
-exports.verify = function(hash, b64_sig, b64_pub_key){
-	try{
-		var signature = new Buffer(b64_sig, "base64"); // 64 bytes (32+32)
-		return ecdsa.verify(hash, signature, new Buffer(b64_pub_key, "base64"));
+
+function sign( hash, priv_key )
+{
+	let res = ecdsa.sign( hash, priv_key );
+	return res.signature.toString( "base64" );
+}
+
+function verify( hash, b64_sig, b64_pub_key )
+{
+	try
+	{
+		//	64 bytes (32+32)
+		let signature = new Buffer( b64_sig, "base64" );
+		return ecdsa.verify( hash, signature, new Buffer( b64_pub_key, "base64" ) );
 	}
-	catch(e){
-		log.consoleLog('signature verification exception: '+e.toString());
+	catch( e )
+	{
+		log.consoleLog( 'signature verification exception: ' + e.toString() );
 		return false;
 	}
-};
+}
+
+
+
+
+
+/**
+ *	exports
+ */
+exports.sign		= sign;
+exports.verify		= verify;
 
